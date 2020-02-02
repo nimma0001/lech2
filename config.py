5dde7f80ced4765b9963ca089a9d453f133f9cc0
Samyek S <39209724+IcedMonk@users.noreply.github.com>
2020-02-02 15:18:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "911929235:AAHq87KY53IyAMPalLHtdF_wCWOK-PwR7eE"
+  APP_ID = 6
+  API_HASH = "40394531f5561a62ca6f890921283b81"
+  AUTH_CHANNEL = -381450204
